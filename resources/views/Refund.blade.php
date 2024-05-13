28bf8be460b59bd181e3bd45ca5aa76f6eff9580
ChrisRoach1 <c.roach10@yahoo.com>
2024-05-13 19:33:23 -0400

    couple small changes

--- resources/views/Refund.blade.php
+++ resources/views/Refund.blade.php
@@ -4,7 +4,7 @@
             <h1 class="mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 lg:mb-6 md:text-5xl xl:text-6xl dark:text-white">Refund Policy</h1>
             <p class="font-light text-gray-500 md:text-lg xl:text-xl dark:text-gray-400">
                 At ThinkForMe.dev, we are committed to providing a unique and innovative experience through our AI-generated content.
-                We understand that AI, isn't perfect we want you to be fully aware of the risk before making any purchases.
+                AI isn't perfect and we want you to be fully aware of the risk before making any purchases.
                 Due to the high cost of generating this content, we unfortunately cannot offer refunds once credits have been used.
                 We believe in transparency and want you to make an informed decision about our unique service. Your understanding and support mean a lot to us and we look forward to sharing
                 new and exciting features in the future.
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,6 @@
-<footer class="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
+<footer class="mt-10 p-4 md:p-8 lg:p-10 dark:bg-gray-800">
     <div class="mx-auto max-w-screen-xl text-center">
         <a href="#" class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
-            <svg class="mr-2 h-8" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-            </svg>
             ThinkForMe!
         </a>
         <p class="my-6 text-gray-500 dark:text-gray-400">Your 1 stop shop for generating project ideas and custom icons through AI.</p>
